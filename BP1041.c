558fbe32cd06329cdd51c2a586d3fbc703d91e74
ashif2512 <61391944+ashif2512@users.noreply.github.com>
2020-05-14 02:25:47 +0600

    Add files via upload

---
+++ BP1041.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main ()
+{
+    float x, y;
+
+    /*receiving input from the user*/
+    scanf("%f%f", &x, &y);
+
+    /*using conditional logic according to the given condition*/
+    if(x == 0.0 && y == 0.0)
+    {
+        printf("Origem\n");
+    }
+    else if(x > 0.0 && y > 0.0)
+    {
+        printf("Q1\n");
+    }
+    else if(x < 0.0 && y > 0.0)
+    {
+        printf("Q2\n");
+    }
+    else if(x < 0.0 && y < 0.0)
+    {
+        printf("Q3\n");
+    }
+    else if(x > 0.0 && y < 0.0)
+    {
+        printf("Q4\n");
+    }
+    else if((x > 0.0 && y == 0.0) || (x < 0.00 && y == 0.0))
+    {
+        printf("Eixo X\n");
+    }
+    else if((x == 0.0 && y > 0.0) || (x == 0.0 && y < 0.0))
+    {
+        printf("Eixo Y\n");
+    }
+
+
+
+
+
+    return 0;
+}
